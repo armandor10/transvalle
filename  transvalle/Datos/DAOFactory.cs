6c13c2a3082d73a651095c16d2202042b4410f38
armandorp10 <armandorp10@gmail.com>
2015-01-14 05:11:40 +0000

    Preparando entorno

---
+++  transvalle/Datos/DAOFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datos
+{
+   public abstract class DAOFactory
+    {
+       public abstract IPersona getPersonaDAO();
+
+       public static DAOFactory getDAOFactory(string db)
+       {
+           switch (db)
+           {
+               case "mysql":
+                   return new MySqlFactory();
+
+           }
+           return null;
+       }
+    }
+}
---
+++  transvalle/Datos/Interfaces/IPersona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Datos;
+using Entidades;
+
+namespace Datos
+{
+    interface IPersona
+    {
+        Entidades.Persona getPersona(string cedula);
+    }
+}
